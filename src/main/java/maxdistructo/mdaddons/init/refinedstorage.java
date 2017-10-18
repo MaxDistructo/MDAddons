4d8a61f455c1a1138139e8a3ae17a0ec1c3ed4fc
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-10-18 13:24:44 -0500

    Update refinedstorage.java

--- src/main/java/maxdistructo/mdaddons/init/refinedstorage.java
+++ src/main/java/maxdistructo/mdaddons/init/refinedstorage.java
@@ -6,13 +6,21 @@ public static final Item refinedIron;
 
 static{
       refinedIron = Arrays.asList(
-                          new ResourceLocation(refinedstorage:quartz_enriched_iron)
-                          
+                          new ResourceLocation(refinedstorage:quartz_enriched_iron),
+                         .stream().map(Item.REGISTRY::getObject)
+				.filter(item -> item != null)
+				.findAny().orElse(null);
 }
 
 public static void init(){
 if(Loader.isModLoaded("refinedstorage")){
 
+Material quartzRefinedIron = new Material("quartzRefinedIron_mdaddons",TextFormatting.GREY);
+quartzRefinedIron.addTrait(PreSharpened.presharpened);
+quartzRefinedIron.addTrait(TraitMagnetic.traitmagnetic(2));
+quartzRefinedIron.addItem(refinedIron, 1, Material.VALUE_Ingot);
+quartzRefinedIron.setCraftable(true);
+quartzRefinedIron.setRepresentativeItem(refinedIron);
 
 
 }
