e3f50f9ebe6f920c9ffda7615fdb492085ef1ca2
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-10-18 10:05:32 -0500

    Start making refined storage integration

---
+++ src/main/java/maxdistructo/mdaddons/init/refinedstorage.java
@@ -0,0 +1,22 @@
+package maxdistructo.mdaddons.init
+
+public class refinedstorage{
+
+public static final Item refinedIron;
+
+static{
+      refinedIron = Arrays.asList(
+                          new ResourceLocation(refinedstorage:quartz_enriched_iron)
+                          
+}
+
+public static void init(){
+if(Loader.isModLoaded("refinedstorage")){
+
+
+
+}
+
+}
+
+}
